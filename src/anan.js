581ca18b17992d7baaf23b2c169cfca2b9f013bd
Anakonda11 <Xapollon430@gmail.com>
2020-05-30 03:02:30 -0400

    jesus, this actually works

---
+++ src/anan.js
@@ -0,0 +1,4 @@
+const Paragraph = require("./test");
+const ReactDom = require("react-dom");
+
+ReactDom.render(Paragraph, document.querySelector("#root"));
---
+++ src/test.js
@@ -0,0 +1,5 @@
+const React = require("react");
+
+let Paragraph = <p>It actually works</p>;
+
+module.exports = Paragraph;
